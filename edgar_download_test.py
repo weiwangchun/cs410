92aa9eb013831828a29b0bfbba2e2424c60aa0f8
weiwangchun <weiwangchun@gmail.com>
2018-11-05 13:37:54 +1000

    Update edgar_download_test.py

--- edgar_download_test.py
+++ edgar_download_test.py
@@ -4,6 +4,7 @@
 #https://github.com/edouardswiac/python-edgar
 
 import os
+import io
 import shutil
 import sys
 import urllib.request 
@@ -12,19 +13,17 @@ import urllib.error
 
 
 # download index files from EDGAR
-def download_index(start_year, end_year, dir_stem, URLstem):
+def download_index(start_year, end_year, URLstem):
     for year in range(start_year, end_year+1):
         for qtr in range(1, 5):
             currURL = URLstem + str(year) + '/QTR' + str(qtr) + '/master.idx'
             copy_file = str(year) + 'QTR' + str(qtr) + '.idx'
             local_file = 'master.idx'
             print('Downloading ' + str(year) + ' Q' + str(qtr) + ' master index.')
-            print(currURL)
-            print(local_file)
+
             try:
                 response = urllib.request.urlopen(currURL)
                 out_file = open(local_file, 'wb')
-                #shutil.copy(out_file, copy_file)
                 shutil.copyfileobj(response, out_file)
                 out_file.close()
                 os.rename(local_file, copy_file)
@@ -34,11 +33,16 @@ def download_index(start_year, end_year, dir_stem, URLstem):
 
 
 
+# extract 10Q and 10K from master idx
+
+
+
+
+
 if __name__ == '__main__':
 
 	start_year = 2018
 	end_year = 2018
 	URLstem = 'https://www.sec.gov/Archives/edgar/full-index/'
-	dir_stem = 'data/'
 
-	download_index(start_year, end_year, dir_stem, URLstem)
+	download_index(start_year, end_year, URLstem)

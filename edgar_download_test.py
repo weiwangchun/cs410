8248d7c0f69f40b305d2d9f0522a36332c50a45a
weiwangchun <weiwangchun@gmail.com>
2018-11-05 15:13:12 +1000

    Update edgar_download_test.py

--- edgar_download_test.py
+++ edgar_download_test.py
@@ -1,16 +1,27 @@
 
+# EDGAR 10Q and 10K download
+
 
 # references: check out the below
+# -----------------------------------------------
 #https://github.com/edouardswiac/python-edgar
+#https://github.com/Vaslo/CS410_Team32_FinalProj/blob/master/build_edgar_mda_files.py
+
 
 import os
 import io
 import shutil
 import sys
+import csv
+import re
 import urllib.request 
-import urllib.error 
+import urllib.error
+from bs4 import BeautifulSoup
 
 
+# --------------------------------
+# DOWNLOAD MASTER INDEX
+# --------------------------------
 
 # download index files from EDGAR
 def download_index(start_year, end_year, URLstem):
@@ -28,21 +39,120 @@ def download_index(start_year, end_year, URLstem):
                 out_file.close()
                 os.rename(local_file, copy_file)
 
-            except HTTPError:
+            except urllib.error:
                 print('URL not found.')
 
 
+# --------------------------------
+# EXTRACT 10K / 10Q INDEX
+# --------------------------------
 
 # extract 10Q and 10K from master idx
+def extract_from_index(start_year, end_year):
+    result_index = '10X-' + str(start_year) + '-' + str(end_year) + '.idx'
+    file_out = open(result_index, 'w') # open file 
+    for year in range(start_year, end_year+1):
+        for qtr in range(1, 5):
+            index_name = str(year) + 'QTR' + str(qtr) + '.idx'
+            try:
+                with io.open(index_name, 'r', encoding='latin-1') as file_in:
+                    print('    ' + str(year) + ' Q' + str(qtr) + '... Found ' + str(extract10x(file_in, file_out)) + ' filings')
+            except IOError:
+                continue
+    file_out.close()
 
+# extract 10K and 10Q -- fix this function
+def extract10x(index_file_handle, output_file_handle):
+
+    count10x = 0
+    for line in index_file_handle:
+        if line.find('.txt') > 0:
+            items = line.split('|')
+            if items[2] in ('10-Q', '10-K'):
+            #if items[2] == '10-Q':
+                output_file_handle.write(line)
+                count10x += 1
+    return count10x
+
+
+
+# --------------------------------
+# DOWNLOAD FILES
+# --------------------------------
+
+# download all EDGAR files listed in the index
+def extract_files_from_index(index_name):
+    filing_items = ()
+    filing_list = []
+
+    # open index file as csv file - go through each row
+    with open(index_name, 'r') as csv_file:
+        filings = csv.reader(csv_file, delimiter ="|")
+        for row in filings:
+            for item in row:
+                filing_items += (item, )
+            tmp = EDGAR_file(filing_items)
+
+            # also save the raw file - we probably don't need this later on - just good to have a look
+            tmp_file = open('files/' + tmp.cik + tmp.company_name + tmp.filing_date + '.txt','w')
+            tmp_file.write(str(tmp.text_clean))
+            tmp_file.close()
+
+            tmp.text_raw
+
+            filing_list.append(tmp)
+            filing_items = ()
+
+    return filing_list
+
+
+
+
+class EDGAR_file:
+    # some attributes
+    text_raw = None
+    text_clean = None
+    text_mda = None
+
+    def __init__(self, filing_items):
+        (self.cik,
+        self.company_name,
+        self.form_type,
+        self.filing_date,
+        self.filing_url) = filing_items
+        self.filing_url = self.filing_url
+        self.get_file()
+        self.clean_text()
+
+    def get_file(self):
+        print('Fetching file ...')
+        try:
+            tmp = urllib.request.urlopen('http://www.sec.gov/Archives/' + self.filing_url)
+            self.text_raw = tmp.read()
+        except urllib.error:
+            print('\t failed.')
+            self.text_raw = ''
+
+    def clean_text(self):
+        text = BeautifulSoup(self.text_raw, 'html.parser')
+        self.text_clean = re.sub(r'[^\x00-\x7F]+|\W{2,}', ' ', text.document.get_text())
+
+    
 
 
 
 
 if __name__ == '__main__':
 
-	start_year = 2018
-	end_year = 2018
-	URLstem = 'https://www.sec.gov/Archives/edgar/full-index/'
+    start_year = 2018
+    end_year = 2018
+    URLstem = 'https://www.sec.gov/Archives/edgar/full-index/'
 
-	download_index(start_year, end_year, URLstem)
+    # (1) download master index files
+    #download_index(start_year, end_year, URLstem)
+
+    # (2) build 10Q 10K index file
+    #extract_from_index(start_year, end_year)
+
+    # (3) extract files and clean txt
+    extract_files_from_index('10X-2018-2018.idx')
